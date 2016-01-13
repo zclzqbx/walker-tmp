ff6946228494e22c26abe7a2fe395da3c8e1589e
zcl <zclzqbx@163.com>
2016-01-13 22:32:15 +0800

    scenario_generate

---
+++ scenario/scenario_tree/scenario_generate/scenario_generate_limit_scenario.cpp
@@ -0,0 +1,76 @@
+/*              极限场景法
+ *author:朱传林
+ *date:2016-01-13
+ *function:输入预测值，输出上下极限场景
+ *
+ */
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <math.h>
+#include <cstdlib>
+using namespace std;
+
+#define POS pos_095
+
+const int NT         = 24;//误差场景时段
+const double pref    = 300;//切割点
+
+//标准正态分布分位点
+const double pos_095 = 1.6449;
+const double pos_090 = 1.2816;
+
+
+
+double topPos(const double b=0.0475,const double u=0)
+{//上极限场景
+	return (b * POS + u);
+}
+
+double bottomPos(const double b=0.0475,const double u=0)
+{//下极限场景
+	return (-1 * b * POS + u);
+}
+
+
+int main()
+{
+	double Pre[NT],u = 0,b = 0.0475;//预测场景、均值、方差
+	double Wz = 350;//总装机容量
+	
+	ifstream input("wind_power.txt",ios::in);
+	if(!input)
+	{
+		cerr<<"lack of file wind_power.txt!!!"<<endl;
+		return -1;
+	}
+	for(int i=0;i<NT;++i)
+	{//读取风电出力预测值
+		input>>Pre[i];
+	}
+	
+	ofstream output("mc_scenarios.txt",ios::ate);
+	for(int k = 0;k < NT;++k)
+	{
+		b = Pre[k] / 5 + Wz / 50;
+		double tmp = Pre[k] + topPos(b,u);//生成一个出力场景
+		if(tmp > pref)
+		{//对生成的场景添加出力上限
+			tmp = pref;
+		}
+		output<<tmp<<"   ";//输出
+	}
+	output<<endl;
+	
+	for(int k = 0;k < NT;++k)
+	{
+		b = Pre[k] / 5 + Wz / 50;
+		double tmp = Pre[k] + bottomPos(b,u);//生成一个出力场景
+		output<<tmp<<"   ";//输出
+	}
+	output<<endl;
+	
+	input.close();
+	output.close();
+	return 0;
+}
